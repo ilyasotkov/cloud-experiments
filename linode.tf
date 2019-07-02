c76a544ec6fce208591af4291b009b33ddb5d945
Ilya Sotkov <ilya@sotkov.com>
2019-07-02 20:11:19 +0300

    Improve naming and add a domain (registered on Namecheap for $1)

--- linode.tf
+++ linode.tf
@@ -5,15 +5,15 @@ resource "linode_sshkey" "controller" {
   label   = "controller"
 }
 
-resource "linode_instance" "swarm" {
-  label  = "swarm"
-  type   = "g6-nanode-1"
-  region = "eu-central"
-
-  group             = "swarm"
-  tags              = ["swarm"]
+resource "linode_instance" "swarm_node" {
+  label             = "swarm_node"
+  type              = "g6-nanode-1"
+  region            = "eu-central"
   private_ip        = false
   boot_config_label = "boot"
+
+  group = "swarm"
+  tags  = ["swarm_node"]
 
   disk {
     label           = "boot"
@@ -35,6 +35,23 @@ resource "linode_instance" "swarm" {
   }
 }
 
-output "ip_address" {
-  value = "${linode_instance.swarm.ip_address}"
+resource "linode_domain" "flexp_xyz" {
+  type      = "master"
+  domain    = "flexp.xyz"
+  soa_email = "soa@flexp.xyz"
+}
+
+resource "linode_domain_record" "arch_flexp_xyz" {
+  domain_id   = "${linode_domain.flexp_xyz.id}"
+  name        = "arch"
+  record_type = "A"
+  target      = "arch.flexp.xyz"
+  ttl_sec     = 300
+  expire_sec  = 300
+  retry_sec   = 300
+  refresh_sec = 300
+}
+
+output "swarm_node_ip_address" {
+  value = "${linode_instance.swarm_node.ip_address}"
 }

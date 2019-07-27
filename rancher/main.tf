f7e27e44f2ff3688b864b1bd32d5e3ea8e48919a
Ilya Sotkov <ilya@sotkov.com>
2019-07-27 22:25:59 +0300

    Add openldap authentication for rancher

--- rancher/main.tf
+++ rancher/main.tf
@@ -1,4 +1,5 @@
 variable "rancher_token_key" {}
+variable "ldap_service_account_password" {}
 
 provider "rancher2" {
   api_url   = "https://rancher-server"
@@ -8,5 +9,16 @@ provider "rancher2" {
 
 resource "rancher2_setting" "auth-user-info-max-age-seconds" {
   name  = "auth-user-info-max-age-seconds"
-  value = "3600"
+  value = "1800"
+}
+
+resource "rancher2_auth_config_openldap" "openldap" {
+  enabled                            = true
+  access_mode                        = "unrestricted"
+  servers                            = ["openldap"]
+  port                               = 389
+  service_account_distinguished_name = "cn=admin,dc=example,dc=org"
+  service_account_password           = var.ldap_service_account_password
+  user_search_base                   = "ou=rancher,dc=example,dc=org"
+  user_login_attribute               = "cn"
 }

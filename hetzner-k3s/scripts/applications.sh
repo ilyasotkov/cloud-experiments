db1695bd0bf61c9f4644617a200e68624330e652
Ilya Sotkov <ilya@sotkov.com>
2019-08-01 09:13:36 +0300

    Use tillerless helm plugin

--- hetzner-k3s/scripts/applications.sh
+++ hetzner-k3s/scripts/applications.sh
@@ -11,9 +11,9 @@ master_ip=$(cat /tmp/tfout.json | jq -r .node_ip_addresses.value[0])
 application_domain_zone=$(cat /tmp/tfout.json | jq -r .domain_zone.value)
 cd -
 
-flags="--quiet --state-values-set masterIpAddress=$master_ip,applicationDomainZone=$application_domain_zone"
-
 export KUBECONFIG=$(pwd)/kubeconfigs/$1.yaml
+
+flags="--quiet --state-values-set masterIpAddress=$master_ip,applicationDomainZone=$application_domain_zone"
 
 if [ $2 == all ]; then
     helmfile $flags --file ./resources/helmfile.yaml sync --concurrency=1
--- hetzner-k3s/scripts/cluster.sh
+++ hetzner-k3s/scripts/cluster.sh
@@ -18,7 +18,5 @@ cd ./ansible; ansible-playbook cluster.yml; cd -
 # Deploy applications onto the cluster
 export KUBECONFIG=$(pwd)/kubeconfigs/$1.yaml
 k3s kubectl apply -f ./resources/init
-helm init --service-account tiller --upgrade --history-max=5 --wait
-
 k3s kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta2/aio/deploy/recommended.yaml
 ./scripts/applications.sh dev all

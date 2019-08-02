e5ca907cf51bb0457b02e3768a0c841a30b0d3a7
Ilya Sotkov <ilya@sotkov.com>
2019-08-02 08:46:02 +0300

    Use external-dns for application domain name management

--- hetzner-k3s/terraform/dns.tf
+++ hetzner-k3s/terraform/dns.tf
@@ -2,11 +2,6 @@ variable "env" {
   type = string
 }
 
-variable "application_domain_names" {
-  type    = list
-  default = ["app"]
-}
-
 variable "domain_zone" {
   type = string
 }
@@ -26,16 +21,6 @@ resource "cloudflare_record" "node_hostnames" {
   domain = var.domain_zone
   name   = "${element(hcloud_server.nodes[*].name, count.index)}${local.domain_suffix}"
   value  = element(hcloud_server.nodes[*].ipv4_address, count.index)
-  type   = "A"
-  ttl    = 1
-}
-
-resource "cloudflare_record" "application" {
-  count = length(var.application_domain_names)
-
-  domain = var.domain_zone
-  name   = element(var.application_domain_names, count.index)
-  value  = element(hcloud_server.nodes[*].ipv4_address, 0)
   type   = "A"
   ttl    = 1
 }

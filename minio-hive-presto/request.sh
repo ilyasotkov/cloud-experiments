5e2484931bd0a9c724b20c3f9f6c784482140728
Ilya Sotkov <ilya@sotkov.com>
2020-01-03 03:17:01 +0200

    Make presto work as well

--- minio-hive-presto/request.sh
+++ minio-hive-presto/request.sh
@@ -6,13 +6,13 @@ curl -s -X PUT -H Content-type:application/json \
 -d '{"comment":"Hello there","location":"s3a://mybucket/mydb","properties":{"a":"b"}}' \
 http://localhost:50111/templeton/v1/ddl/database/mydb1?user.name=ilya
 
-# curl -s -X PUT -H Content-type:application/json -d '{
-#  "comment": "Best table made today",
-#  "columns": [
-#    { "name": "id", "type": "string" },
-#    { "name": "fn", "type": "string" },
-#    { "name": "ln", "type": "string" }
-#   ],
-# "format": { "storedAs": "TEXTFILE" },
-# "location": "s3a://mybucket/mydata/" }' \
-# 'http://localhost:50111/templeton/v1/ddl/database/mydb/table/test_table_000?user.name=ilya'
+curl -s -X PUT -H Content-type:application/json -d '{
+ "comment": "Best table made today",
+ "columns": [
+   { "name": "id", "type": "string" },
+   { "name": "fn", "type": "string" },
+   { "name": "ln", "type": "string" }
+  ],
+"format": { "storedAs": "TEXTFILE" },
+"location": "s3a://mybucket/mydata/" }' \
+'http://localhost:50111/templeton/v1/ddl/database/mydb1/table/test_table_000?user.name=ilya'
